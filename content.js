b2429fcbf7312fd75c6e5639e0858d68e6215308
sytkm <sytkm@sytkm-MacBookPro.local>
2018-05-02 01:31:40 +0900

    initial commit

---
+++ content.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  console.log("aaa");
+  $('map').imageMapResize();
+});
+
+$(function() {
+  var baseX = $('#parallax').offset().left + 180;
+  var baseY = $('#parallax').offset().top  + 240;
+  var layers = $('#parallax').children().map(function(_, layer) {
+    return {
+      $: $(layer),
+      factor:$(layer).data("z")/8000,
+      baseX: $(layer).position().left,
+      baseY: $(layer).position().top
+    };
+  });
+  $(document.body).mousemove(function(ev) {
+    $.each(layers, function(_, layer) {
+      var dx = ev.pageX - baseX;
+      var dy = ev.pageY - baseY;
+      layer.$.css({
+        left: layer.baseX + (dx*layer.factor) +"px",
+        top:  layer.baseY + (dy*layer.factor) +"px" });
+      });
+    });
+    var isSmartphone = navigator.userAgent.search(/(iPhone|iPad|Android)/) !== -1;
+    !isSmartphone && $(document.body).mousemove(function(ev) {
+      var dx = ev.pageX - baseX;
+      var dy = ev.pageY - baseY;
+      $.each(layers, function(_, layer) {
+        layer.$.css({
+          left: layer.baseX + (dx*layer.factor) +"px",
+          top:  layer.baseY + (dy*layer.factor) +"px" });
+        });
+      });
+      // 今回追加したコード
+      isSmartphone && window.addEventListener("devicemotion", function(ev){
+        var dy = baseY/2 + ev.accelerationIncludingGravity.y*100;
+        var dx = baseX/2 - ev.accelerationIncludingGravity.x*100;
+        $.each(layers, function(_, layer) {
+          layer.$.css({
+            left: layer.baseX + (dx*layer.factor) +"px",
+            top:  layer.baseY + (dy*layer.factor) +"px" });
+          });
+        });
+      });
